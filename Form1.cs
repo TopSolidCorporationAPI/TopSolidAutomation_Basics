240568a95f87755935364a64762306bf861bddb1
Anne-françoise STUYK <af.stuyk@afbimservices.fr>
2024-11-13 15:46:27 +0100

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using TopSolid.Kernel.Automating;
+using TopSolid.Cad.Design.Automating;
+using TopSolid.Cad.Drafting.Automating;
+
+namespace MyAutomationApplication
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //checks TopSolid is connected before performing any action
+            if (TopSolidHost.IsConnected)
+            {
+                //fetch topsolid version from application interface
+                string currentTopSolidVersion = TopSolidHost.Application.Version.ToString();
+
+                //display the value into a message box
+                MessageBox.Show(currentTopSolidVersion, "TopSolid Version", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using TopSolid.Kernel.Automating;
+using TopSolid.Cad.Design.Automating;
+using TopSolid.Cad.Drafting.Automating;
+
+namespace MyAutomationApplication
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            //connect
+            TopSolidHost.Connect();
+            TopSolidDesignHost.Connect();
+            TopSolidDraftingHost.Connect();
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+
+            //disconnect
+            TopSolidHost.Disconnect();
+            TopSolidDesignHost.Disconnect();
+            TopSolidDraftingHost.Disconnect();
+        }
+    }
+}
